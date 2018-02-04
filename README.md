11a8229536db26907b01431d6e75224d7241373f
jtothe4n <jantay.duong@googlemail.com>
2018-02-04 12:32:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pixel 2 and Pixel 2 XL - Active Edge Magisk Module for Edge Sense Plus
+
+Replaces the SystemUIGoogle.apk that hosts the Elmyra (Active Edge) service with a modified version.
+It hooks into the squeeze events and forwards it to the app Edge Sense Plus which provides tons of different actions to choose.
+You will also have a long squeeze and a double squeeze gesture, as well as seperate squeeze gestures based on what position the device is,
+like flat on the table or in landscape mode.
---
+++ config.sh
@@ -0,0 +1,124 @@
+##########################################################################################
+#
+# Magisk Module Template Config Script
+# by topjohnwu
+#
+##########################################################################################
+##########################################################################################
+#
+# Instructions:
+#
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (config.sh)
+# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
+# 5. Add your additional or modified system properties into common/system.prop
+#
+##########################################################################################
+
+##########################################################################################
+# Configs
+##########################################################################################
+
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=false
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=false
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "    Active Edge System Mod     "
+  ui_print "*******************************"
+}
+
+##########################################################################################
+# Replace list
+##########################################################################################
+
+# List all directories you want to directly replace in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
+
+# This is an example
+REPLACE="
+"
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+set_permissions() {
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Here are some examples for the set_perm functions:
+
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+}
+
+##########################################################################################
+# Custom Functions
+##########################################################################################
+
+# This file (config.sh) will be sourced by the main flash script after util_functions.sh
+# If you need custom logic, please add them here as functions, and call these functions in
+# update-binary. Refrain from adding code directly into update-binary, as it will make it
+# difficult for you to migrate your modules to newer template versions.
+# Make update-binary as clean as possible, try to only do function calls in it.
+
+install_module() {
+
+
+	if [ "`getprop ro.product.device`" = "walleye" ]; then
+		
+		unzip -o "$ZIP" 'walleye/*' -d $INSTALLER 2>/dev/null
+		
+		ui_print "- Extracting module files for walleye"
+
+		APK_PATH=$INSTALLER/walleye/priv-app/SystemUIGoogle/SystemUIGoogle.apk
+		
+		mkdir -p $MODPATH/system/priv-app/SystemUIGoogle 2>/dev/null
+		cp -af $APK_PATH $MODPATH/system/priv-app/SystemUIGoogle/SystemUIGoogle.apk
+	fi
+
+	if [ "`getprop ro.product.device`" = "taimen" ]; then
+		unzip -o "$ZIP" 'taimen/*' -d $INSTALLER 2>/dev/null
+	
+		ui_print "- Extracting module files for taimen"
+		
+		APK_PATH=$INSTALLER/taimen/priv-app/SystemUIGoogle/SystemUIGoogle.apk
+		mkdir -p $MODPATH/system/priv-app/SystemUIGoogle 2>/dev/null
+		cp -af $APK_PATH $MODPATH/system/priv-app/SystemUIGoogle/SystemUIGoogle.apk
+	fi
+
+	
+	if [ "`getprop ro.product.device`" != "walleye" ] && [ "`getprop ro.product.device`" != "taimen" ]; then
+	
+		abort "- Incompatible device!"
+	fi
+
+  
+}
+
