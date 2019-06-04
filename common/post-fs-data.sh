eb49741c4cfffcea2adf9fbd2ed9d60e333d7e13
jtothe4n <jantay.duong@googlemail.com>
2019-06-04 13:26:53 +0200

    [ADD] Bootloop protection with compatibility check in post-fs-data

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,3 +5,13 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+
+SUPPORTED_PATCH_VERSION="2019-06-05"
+SECURITY_PATCH_VERSION=`getprop ro.build.version.security_patch`
+
+rm -f $MODDIR/disable
+
+if [ $SUPPORTED_PATCH_VERSION \< $SECURITY_PATCH_VERSION ]; then
+	touch $MODDIR/disable
+fi
