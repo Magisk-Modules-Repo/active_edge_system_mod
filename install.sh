690bd34a8e34984fd3320165674eeca3553c4539
j_to_the_4n <jantay.duong@googlemail.com>
2019-12-16 07:10:20 +0100

    [FIX] Potential booloops when the module is installed before the new patch level.
    Co-Authored-By: Rhyse Simpson <skittles9832@gmail.com>

--- install.sh
+++ install.sh
@@ -131,80 +131,78 @@ print_modname() {
 # Copy/extract your module files into $MODPATH in on_install.
 
 on_install() {
-  
-	ui_print "______________________________"
-	ui_print " "
-	ui_print "Installing Active Edge Module"
-	
-	DEVICE=`getprop ro.product.device`
-	RELEASE=`getprop ro.build.version.release`
-	SECURITY_PATCH_VERSION=`getprop ro.build.version.security_patch`
-	ui_print " "
+    ui_print "______________________________"
+    ui_print " "
+    ui_print "Installing Active Edge Module"
+
+    DEVICE=`getprop ro.product.device`
+    RELEASE=`getprop ro.build.version.release`
+    SECURITY_PATCH_VERSION=`getprop ro.build.version.security_patch`
+    ui_print " "
 
 
-	ui_print "Device: "$DEVICE
-	ui_print "Android version: "$RELEASE
-	ui_print "Security patch version: "$SECURITY_PATCH_VERSION
-	ui_print " "
-	
-	if [ $DEVICE != "walleye" ] && [ $DEVICE != "taimen" ] && [ $DEVICE != "crosshatch" ] && [ $DEVICE != "blueline" ] && [ $DEVICE != "bonito" ] && [ $DEVICE != "sargo" ] && [ $DEVICE != "coral" ] && [ $DEVICE != "flame" ]; then
-		abort " => Device '"$DEVICE"' is not supported"
-	fi
-	
-	if [ $RELEASE != "9" ] && [ $RELEASE != "Q" ] && [ $RELEASE != "10" ]; then
-		abort "   => Android version '"$RELEASE"' is not supported"
-	fi
-	
-	RELEASEFOLDER=$RELEASE
-	
-	if [ $RELEASE != "8.1.0" ]; then
-	 	RELEASEFOLDER=$RELEASE/$SECURITY_PATCH_VERSION
-	fi
-	
-	unzip -o "$ZIPFILE" curl -d $TMPDIR
-	cp -f $TMPDIR/curl $MODPATH/curl
-	chmod 0755 $MODPATH/curl
-	
-	APK_PATH=$RELEASEFOLDER/$DEVICE/priv-app/SystemUIGoogle/SystemUIGoogle.apk
+    ui_print "Device: "$DEVICE
+    ui_print "Android version: "$RELEASE
+    ui_print "Security patch version: "$SECURITY_PATCH_VERSION
+    ui_print " "
 
-	TARGETPATH=$MODPATH/system/priv-app/SystemUIGoogle
-	
-	if [ $RELEASE == "Q" ] || [ $RELEASE == "10" ]; then
-		TARGETPATH=$MODPATH/system/product/priv-app/SystemUIGoogle
-	fi
-	
-	URL=https://github.com/Magisk-Modules-Repo/active_edge_system_mod/raw/Files/$APK_PATH
-	ui_print "Downloading SystemUIGoogle.apk for your device..."
+    if [ $DEVICE != "walleye" ] && [ $DEVICE != "taimen" ] && [ $DEVICE != "crosshatch" ] && [ $DEVICE != "blueline" ] && [ $DEVICE != "bonito" ] && [ $DEVICE != "sargo" ] && [ $DEVICE != "coral" ] && [ $DEVICE != "flame" ]; then
+      abort " => Device '"$DEVICE"' is not supported"
+    fi
 
-	mkdir -p $TARGETPATH
-  
-	$MODPATH/curl -k -L $URL -o $TARGETPATH/SystemUIGoogle.apk
-  
-	if [ ! -f $TARGETPATH/SystemUIGoogle.apk ]; then
-		ui_print "Download FAILED: "$URL
-		ui_print ""
-		ui_print "Possible reasons:"
-		ui_print " => Github servers are sometimes unreliable. Check the changelog if the current security patch is supported. If that is the case try to install the module again till the download succeeds"
-		abort " => The update with security patch version '"$SECURITY_PATCH_VERSION"' is not supported yet. In that case please wait for the module to be updated."
-	fi	
-	
-	$MODPATH/curl -k -L $URL.md5 -o $TMPDIR/SystemUIGoogle.apk.md5
-	read -r RMD5 < $TMPDIR/SystemUIGoogle.apk.md5
-	
-	
+    if [ $RELEASE != "9" ] && [ $RELEASE != "Q" ] && [ $RELEASE != "10" ]; then
+      abort "   => Android version '"$RELEASE"' is not supported"
+    fi
+
+    RELEASEFOLDER=$RELEASE
+
+    if [ $RELEASE != "8.1.0" ]; then
+      RELEASEFOLDER=$RELEASE/$SECURITY_PATCH_VERSION
+    fi
+
+    unzip -o "$ZIPFILE" curl -d $TMPDIR
+    cp -f $TMPDIR/curl $MODPATH/curl
+    chmod 0755 $MODPATH/curl
+
+    APK_PATH=$RELEASEFOLDER/$DEVICE/priv-app/SystemUIGoogle/SystemUIGoogle.apk
+
+    TARGETPATH=$MODPATH/system/priv-app/SystemUIGoogle
+
+    if [ $RELEASE == "Q" ] || [ $RELEASE == "10" ]; then
+      TARGETPATH=$MODPATH/system/product/priv-app/SystemUIGoogle
+    fi
+
+    URL=https://github.com/Magisk-Modules-Repo/active_edge_system_mod/raw/Files/$APK_PATH
+    ui_print "Downloading SystemUIGoogle.apk for your device..."
+
+    mkdir -p $TARGETPATH
+
+    $MODPATH/curl -k -L $URL -o $TARGETPATH/SystemUIGoogle.apk
+
+    if [ ! -f $TARGETPATH/SystemUIGoogle.apk ]; then
+      ui_print "Download FAILED: "$URL
+      ui_print ""
+      ui_print "Possible reasons:"
+      ui_print " => Github servers are sometimes unreliable. Check the changelog if the current security patch is supported. If that is the case try to install the module again till the download succeeds"
+      abort " => The update with security patch version '"$SECURITY_PATCH_VERSION"' is not supported yet. In that case please wait for the module to be updated."
+    fi	
+
+    $MODPATH/curl -k -L $URL.md5 -o $TMPDIR/SystemUIGoogle.apk.md5
+    read -r RMD5 < $TMPDIR/SystemUIGoogle.apk.md5
+
     LMD5="$(md5sum "$TARGETPATH/SystemUIGoogle.apk" | cut -d ' ' -f 1)"
-	
-	ui_print "Local MD5: "$LMD5
-	ui_print "REMOTE MD5: "$RMD5
-	
-	if [ ! "$LMD5" == "$RMD5" ]; then
+
+    ui_print "Local MD5: "$LMD5
+    ui_print "REMOTE MD5: "$RMD5
+
+    if [ ! "$LMD5" == "$RMD5" ]; then
       abort "MD5 of downloaded file does not match the remote file! Please try to flash the module again. Aborting."
     fi
-	
-	
-	ui_print " "
-	ui_print "SUCCESSFULLY INSTALLED."
-	ui_print " "
+
+    sed -i "s/INSTALLVAR/$(getprop ro.build.version.security_patch | tr -d "-")/" $TMPDIR/post-fs-data.sh
+    ui_print " "
+    ui_print "SUCCESSFULLY INSTALLED."
+    ui_print " "
 }
 
 # Only some special files require specific permissions
